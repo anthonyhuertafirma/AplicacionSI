a1ac0a7aeef730c8515d597084b4a8405c305f08
Pablo Guerra Camana <pguerracamana@gmail.com>
2025-05-13 17:48:10 -0500

    Terminado modelo exportable a http

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
+import joblib
 from fastapi import FastAPI
 
+from models.PhoneInput import PhoneInput
+
 app = FastAPI()
+model = joblib.load("./notebooks/naive_bayes_model.joblib")
 
 
 @app.get("/")
@@ -11,3 +15,40 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/predict")
+async def root(specs: PhoneInput):
+    input_data = [[
+        specs.battery_power,
+        specs.blue,
+        specs.clock_speed,
+        specs.dual_sim,
+        specs.fc,
+        specs.four_g,
+        specs.int_memory,
+        specs.m_dep,
+        specs.mobile_wt,
+        specs.n_cores,
+        specs.pc,
+        specs.px_height,
+        specs.px_width,
+        specs.ram,
+        specs.sc_h,
+        specs.sc_w,
+        specs.talk_time,
+        specs.three_g,
+        specs.touch_screen,
+        specs.wifi
+    ]]
+
+    prediction = model.predict(input_data)
+
+    if prediction == 0:
+        return "Gama baja"
+    elif prediction == 1:
+        return "Gama media"
+    elif prediction == 2:
+        return "Gama media alta"
+    else:
+        return "Gama alta"
